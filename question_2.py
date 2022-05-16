e420be2d1e84a0767d62dced4a193d6143c3f8a9
guruamit <105117009+guruamit@users.noreply.github.com>
2022-05-17 00:22:00 +0530

    Add files via upload

---
+++ question_2.py
@@ -0,0 +1,9 @@
+# Write a Python program that accepts a word from the user and reverse it.
+
+
+
+reverse = input("Input the word you want to reverse :")
+
+for char in range(len(reverse) - 1, -1, -1):
+  print(reverse[char], end="")
+print("\n")
